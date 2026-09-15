693eb0ff0644efbd743e36d1730ab55aac3db5e4
Alisher <alisherxujanov163@gmail.com>
2026-09-15 17:11:36 +0500

    Refactor layout structure and styles: replace layout.js with layout.jsx, update page structure in page.jsx, and simplify global styles in globals.css. Remove unused CSS and components for a cleaner codebase.

---
+++ fullstack-frontend/src/app/page.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <>
+            <h1>Hello world</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla illo cupiditate culpa ipsa ratione non sunt consequuntur doloremque quod quia? Consequatur impedit eaque dignissimos rerum, odio maxime officia cumque quae.</p>
+        </>
+    )
+}
+export default Home
